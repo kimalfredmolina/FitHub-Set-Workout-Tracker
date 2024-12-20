41b7f5c7fe46e0560d85ce6c39281ab52bc0657e
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2024-12-20 21:07:24 +0800

    Not added from previous push

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,10 @@ dependencies {
 
     implementation("com.google.android.gms:play-services-auth:20.7.0")
 
+    implementation ("com.github.bumptech.glide:glide:4.15.1")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.15.1")
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
