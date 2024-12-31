fbd3628a624b48c725583a9860ca623fc0a31b56
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2024-12-31 16:02:27 +0800

    Added Layout for alarm no function yet

--- app/src/main/java/com/example/fithub_set_workout_tracker/main_pages/HomePage.java
+++ app/src/main/java/com/example/fithub_set_workout_tracker/main_pages/HomePage.java
@@ -1,28 +1,22 @@
 package com.example.fithub_set_workout_tracker.main_pages;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.fragment.app.Fragment;
+
+import com.example.fithub_set_workout_tracker.Alarm;
 import com.example.fithub_set_workout_tracker.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomePage#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomePage extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -30,15 +24,6 @@ public class HomePage extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomePage.
-     */
-    // TODO: Rename and change types and number of parameters
     public static HomePage newInstance(String param1, String param2) {
         HomePage fragment = new HomePage();
         Bundle args = new Bundle();
@@ -60,7 +45,13 @@ public class HomePage extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_page, container, false);
+        View view = inflater.inflate(R.layout.fragment_home_page, container, false);
+        Button alarmButton = view.findViewById(R.id.alarm_btn);
+        alarmButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), Alarm.class);
+            startActivity(intent);
+        });
+
+        return view;
     }
 }
